3abd65bfa5221493468358452365cf3f21cc7c7d
Roman <claprood.8@osu.edu>
2015-02-12 12:47:31 -0500

    Update findSet

--- findSet.rb
+++ findSet.rb
@@ -13,11 +13,11 @@ def findSet(shownCards)
 	
 	#z is used as a test to make sure that there are enough iterations. for example if there are 12 cards shown, then z should read 12c3 (12 choose 3) which would be 220
 	z = 0
-	while i < (shownCards.size)-2 and !hasSet
+	while i < (shownCards.deckSize)-2 and #!hasSet
 		j = i +1
-		while j < (shownCards.size)-1 and !hasSet
+		while j < (shownCards.deckSize)-1 and #!hasSet
 			k = j + 1
-			while k < (shownCards.size) and !hasSet
+			while k < (shownCards.deckSize) and #!hasSet
 				hasSet = checkSet(shownCards, i, j, k)
 				z += 1
 				puts "#{z}"
