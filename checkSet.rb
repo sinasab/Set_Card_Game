25205eb910deb87ed59292fd3fa1e70a2d41fd27
Zach Spieler <spieler.3@osu.edu>
2015-02-12 03:23:24 -0500

    Update checkSet

--- checkSet.rb
+++ checkSet.rb
@@ -1,24 +1,12 @@
-require_relative './card'
-
+require_relative './card.rb'
+require_relative './deck.rb'
 
 def checkSetColor(cardDeck, x, y, z)
 	colorCheck = 0;
 	
-	# puts "Enter first card number: "
-	# x = gets.chomp
-	
-	# puts "Enter second card number: "
-	# y = gets.chomp
-	
-	# puts = "Enter third card number: "
-	# z = gets.chomp
 
-
-	# puts "#{x} #{y} #{z}"
-
-	if (cardDeck[x.to_i].color == cardDeck[y.to_i].color and cardDeck[z.to_i].color == cardDeck[y.to_i].color) or ((cardDeck[x.to_i].color != cardDeck[y.to_i].color) and (cardDeck[x.to_i].color != cardDeck[z.to_i].color) and (cardDeck[y.to_i].color != cardDeck[z.to_i].color))
-		colorCheck = 1  #will eventually be replaced by 1
-		#puts "#{colorCheck}"
+	if (cardDeck.pickACard(x).color == cardDeck.pickACard(y).color and cardDeck.pickACard(z).color == cardDeck.pickACard(y).color) or ((cardDeck.pickACard(x).color != cardDeck.pickACard(y).color) and (cardDeck.pickACard(x).color != cardDeck.pickACard(z).color) and (cardDeck.pickACard(y).color != cardDeck.pickACard(z).color))
+		colorCheck = 1  
 	
 	end
 
@@ -30,25 +18,8 @@ def checkSetShape(cardDeck, x, y, z)
 	
 	shapeCheck = 0;
 
-	# puts "Enter first card number: "
-	# x = gets.chomp
-	
-	# puts "Enter second card number: "
-	# y = gets.chomp
-	
-	# puts = "Enter third card number: "
-	# z = gets.chomp
-
-
-
-	# puts "#{x} #{y} #{z}"
-
-	# puts "#{cardDeck[x.to_i].shape} #{cardDeck[y.to_i].shape} #{cardDeck[z.to_i].shape}"
-
-
-	if (cardDeck[x.to_i].shape == cardDeck[y.to_i].shape and cardDeck[z.to_i].shape == cardDeck[y.to_i].shape) or ((cardDeck[x.to_i].shape != cardDeck[y.to_i].shape) and (cardDeck[x.to_i].shape != cardDeck[z.to_i].shape) and (cardDeck[y.to_i].shape != cardDeck[z.to_i].shape))
-		shapeCheck = 1  #will be replaced by 1 
-		#puts "#{shapeCheck}"
+	if (cardDeck.pickACard(x).shape == cardDeck.pickACard(y).shape and cardDeck.pickACard(z).shape == cardDeck.pickACard(y).shape) or ((cardDeck.pickACard(x).shape != cardDeck.pickACard(y).shape) and (cardDeck.pickACard(x).shape != cardDeck.pickACard(z).shape) and (cardDeck.pickACard(y).shape != cardDeck.pickACard(z).shape))
+		shapeCheck = 1  
 	end
 	return shapeCheck
 
@@ -57,24 +28,9 @@ end
 def checkSetNumber(cardDeck, x, y, z)
 	
 	numberCheck = 0;
-	
-	# puts "Enter first card number: "
-	# x = gets.chomp
-	
-	# puts "Enter second card number: "
-	# y = gets.chomp
-	
-	# puts = "Enter third card number: "
-	# z = gets.chomp
 
-	# puts "#{x} #{y} #{z}"
-
-	# puts "#{cardDeck[x.to_i].number} #{cardDeck[y.to_i].number} #{cardDeck[z.to_i].number}"
-
-	if (cardDeck[x.to_i].number == cardDeck[y.to_i].number and cardDeck[z.to_i].number == cardDeck[y.to_i].number) or ((cardDeck[x.to_i].number != cardDeck[y.to_i].number) and (cardDeck[x.to_i].number != cardDeck[z.to_i].number) and (cardDeck[y.to_i].number != cardDeck[z.to_i].number))
-		numberCheck = 1  #will eventually be replaced with 1	
-		#puts "#{numberCheck}" 
-	
+	if (cardDeck.pickACard(x).number == cardDeck.pickACard(y).number and cardDeck.pickACard(z).number == cardDeck.pickACard(y).number) or ((cardDeck.pickACard(x).number != cardDeck.pickACard(y).number) and (cardDeck.pickACard(x).number != cardDeck.pickACard(z).number) and (cardDeck.pickACard(y).number != cardDeck.pickACard(z).number))
+		numberCheck = 1  
 	end
 	return numberCheck
 end
@@ -83,22 +39,10 @@ def checkSetShade(cardDeck, x, y, z)
 	
 	shadeCheck = 0;
 	
-	# puts "Enter first card number: "
-	# x = gets.chomp
-	
-	# puts "Enter second card number: "
-	# y = gets.chomp
-	
-	# puts = "Enter third card number: "
-	# z = gets.chomp
 
-	# puts "#{x} #{y} #{z}"
 
-	# puts "#{cardDeck[x.to_i].shade} #{cardDeck[y.to_i].shade} #{cardDeck[z.to_i].shade}"
-
-	if (cardDeck[x.to_i].shade == cardDeck[y.to_i].shade and cardDeck[z.to_i].shade == cardDeck[y.to_i].shade) or ((cardDeck[x.to_i].shade != cardDeck[y.to_i].shade) and (cardDeck[x.to_i].shade != cardDeck[z.to_i].shade) and (cardDeck[y.to_i].shade != cardDeck[z.to_i].shade))
-		shadeCheck = 1 #this will eventually be replaced with 1
-		#puts "#{shadeCheck}"
+	if (cardDeck.pickACard(x).shade == cardDeck.pickACard(y).shade and cardDeck.pickACard(z).shade == cardDeck.pickACard(y).shade) or ((cardDeck.pickACard(x).shade != cardDeck.pickACard(y).shade) and (cardDeck.pickACard(x).shade != cardDeck.pickACard(z).shade) and (cardDeck.pickACard(y).shade != cardDeck.pickACard(z).shade))
+		shadeCheck = 1 
 	end
 	return shadeCheck
 end
@@ -119,6 +63,6 @@ def checkSet(cardDeck, card1, card2, card3)
 	sumCheck = 0
 	sumCheck = checkSetShade(cardDeck, card1, card2, card3) + checkSetNumber(cardDeck, card1, card2, card3) + checkSetColor(cardDeck, card1, card2, card3) + checkSetShape(cardDeck, card1, card2, card3)
 
-	return sumCheck==4
+	return sumCheck == 4
 
 end
