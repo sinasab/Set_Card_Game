e539ee091cec9a81ce84872199deb55bef494077
Zach Spieler <spieler.3@osu.edu>
2015-02-15 12:12:57 -0500

    Add all test cases to checkSetTest

--- testing/checkSetTest.rb
+++ testing/checkSetTest.rb
@@ -9,7 +9,22 @@ are dealt.  The array of cards on the table is passed as well as three cards.
 =end
 testDeck = Deck.new
 testDeck.makeDeck
-tableCards =Array.new(testDeck.dealCards)
+
+#modification of dealCards found in the card class. Used to make an array holding all 81 cards in the deck
+def dealCardsTest(testDeck)
+    	tableCards = []
+    	i = 0
+    	#deal cards 3 at a time until the table has at least 12 cards and a set. stop if the deck runs out of cards
+    	while tableCards.length < 81
+        	tableCards.push(testDeck.pickACard(i))
+    	i += 1
+    	end
+    	return tableCards
+end
+
+tableCards = dealCardsTest(testDeck)
+
+
 
 puts "Color Test 1 - 3 different colors: Should Produce 1"
 puts "#{tableCards[0].color}"	#output red
@@ -34,13 +49,106 @@ puts "#{tableCards[0].color}"	#output red
 puts "#{tableCards[3].color}"	#ouptut red
 puts "#{tableCards[6].color}"	#output red
 
-test = checkSetColor(tableCards, 0, 3, 6)	#should produce 0
+test = checkSetColor(tableCards, 0, 3, 6)	#should produce 1
 puts "Output: #{test}"
 puts ""
 
-puts "#{testDeck.pickACard(0).display}"
-puts "#{testDeck.pickACard(1).display}"
-puts "#{testDeck.pickACard(2).display}"
+puts "Shape Test 1  - 3 same: Should Produce 1"
+puts "#{tableCards[0].shape}"	#output diamond
+puts "#{tableCards[1].shape}"	#ouptut diamond
+puts "#{tableCards[2].shape}"	#output diamond
 
-puts "test"
+test = checkSetShape(tableCards, 0, 1, 2)	#should produce 1
+puts "Output: #{test}"
+puts ""
+
+puts "Shape Test 2  - 3 different: Should Produce 1"
+puts "#{tableCards[0].shape}"	#output diamond
+puts "#{tableCards[30].shape}"	#ouptut squiggle
+puts "#{tableCards[80].shape}"	#output oval
+
+test = checkSetShape(tableCards, 0, 30, 80)	#should produce 1
+puts "Output: #{test}"
+puts ""
+
+puts "Shape Test 3  - 2 same, 1 different: Should Produce 0"
+puts "#{tableCards[0].shape}"	#output diamond
+puts "#{tableCards[1].shape}"	#ouptut diamond
+puts "#{tableCards[80].shape}"	#output oval
+
+test = checkSetShape(tableCards, 0, 1, 80)	#should produce 0
+puts "Output: #{test}"
+puts ""
+
+puts "Shade Test 1  - 3 same: Should Produce 1"
+puts "#{tableCards[0].shade}"	#output solid
+puts "#{tableCards[1].shade}"	#ouptut solid
+puts "#{tableCards[2].shade}"	#output solid
+
+test = checkSetShade(tableCards, 0, 1, 2)	#should produce 1
+puts "Output: #{test}"
+puts ""
+
+puts "Shade Test 2  - 3 different: Should Produce 1"
+puts "#{tableCards[0].shade}"	#output solid
+puts "#{tableCards[3].shade}"	#ouptut striped
+puts "#{tableCards[6].shade}"	#output open
+
+test = checkSetShade(tableCards, 0, 3, 6)	#should produce 1
+puts "Output: #{test}"
+puts ""
+
+puts "Shade Test 3  - 2 same, 1 different: Should Produce 0"
+puts "#{tableCards[0].shade}"	#output solid
+puts "#{tableCards[1].shade}"	#ouptut solid
+puts "#{tableCards[3].shade}"	#output striped
+
+test = checkSetShade(tableCards, 0, 1, 3)	#should produce 0
+puts "Output: #{test}"
+puts ""
+
+puts "Number Test 1  - 3 same: Should Produce 1"
+puts "#{tableCards[0].number}"	#output 1
+puts "#{tableCards[1].number}"	#ouptut 1
+puts "#{tableCards[2].number}"	#output 1
+
+test = checkSetNumber(tableCards, 0, 1, 2)	#should produce 1
+puts "Output: #{test}"
+puts ""
+
+puts "Number Test 2  - 3 same: Should Produce 1"
+puts "#{tableCards[0].number}"	#output 1
+puts "#{tableCards[15].number}"	#ouptut 2
+puts "#{tableCards[26].number}"	#output 3
+
+test = checkSetNumber(tableCards, 0, 15, 26)	#should produce 1
+puts "Output: #{test}"
+puts ""
+
+puts "Number Test 3  - 2 same, 1 different: Should Produce 0"
+puts "#{tableCards[0].number}"	#output 1
+puts "#{tableCards[1].number}"	#ouptut 1
+puts "#{tableCards[26].number}"	#output 3
+
+test = checkSetNumber(tableCards, 0, 1, 26)	#should produce 0
+puts "Output: #{test}"
+puts ""
+
+puts "checkSet Test 1: Found a Set, print true"
+puts "#{checkSet(tableCards, 0 ,1 ,2)}"
+
+puts "checkSet Test 1: 3 Match, print false"
+puts "#{checkSet(tableCards, 0 ,1 ,5)}"
+
+puts "checkSet Test 3: 2 match, print false"
+puts "#{checkSet(tableCards, 0 ,1 ,3)}"
+
+puts "checkSet Test 3: 1 match, print false"
+puts "#{checkSet(tableCards, 0 ,80 ,3)}"
+
+puts "checkSet Test 3: 0 match, print false"
+puts "#{checkSet(tableCards, 0 ,17 ,38)}"
+
+
+
 
